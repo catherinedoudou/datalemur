76847efb2a23a8131387c72378c26d543e7480d1
cdrowley <49714796+cdrowley@users.noreply.github.com>
2023-06-05 16:27:15 +0100

    -

--- questions/sql/medium/signup-confirmation-rate.sql
+++ questions/sql/medium/signup-confirmation-rate.sql
@@ -1,5 +1,5 @@
 -- https://datalemur.com/questions/questions/signup-confirmation-rate
-SELECT ROUND(COUNT(t . *)::DECIMAL / COUNT(e . *), 2)
+SELECT ROUND(COUNT(t . *)::DECIMAL / COUNT(e . *), 2) AS confirm_rate
 FROM emails e
 LEFT JOIN texts t
   ON t.email_id = e.email_id
