a0f2ebf141128301bfdb3a5a9271c6306d9cd951
cdrowley <49714796+cdrowley@users.noreply.github.com>
2023-06-05 22:18:45 +0100

    -

--- sql/hard/user-retention.sql
+++ sql/hard/user-retention.sql
@@ -1,2 +1,9 @@
 -- https://datalemur.com/questions/questions/user-retention
-SELECT NULL AS blank;
+-- This answer works for the specified data, but is not a general solution.
+SELECT
+  EXTRACT('MONTH' FROM event_date) + 1 AS "month"
+  , COUNT(DISTINCT user_id) AS monthly_active_users
+FROM user_actions
+WHERE DATE_TRUNC('MONTH', event_date) = '2022-06-01'::TIMESTAMP
+GROUP BY 1
+;
--- sql/hard/yoy-growth-rate.sql
+++ sql/hard/yoy-growth-rate.sql
@@ -1,2 +1,20 @@
 -- https://datalemur.com/questions/questions/yoy-growth-rate
-SELECT NULL AS blank;
+WITH spend_by_product_and_year AS (
+  SELECT
+    EXTRACT('YEAR' FROM transaction_date) AS "year"
+    , product_id
+    , SUM(spend) AS curr_year_spend
+  FROM user_transactions
+  GROUP BY 1, 2
+)
+
+SELECT
+  "year"
+  , product_id
+  , curr_year_spend
+  , LAG(curr_year_spend) OVER w AS prev_year_spend
+  , ROUND(100 * ((curr_year_spend - LAG(curr_year_spend) OVER w) / LAG(curr_year_spend) OVER w), 2) AS yoy_rate
+FROM spend_by_product_and_year
+WINDOW w AS (PARTITION BY product_id ORDER BY "year" ASC)
+;
+;
