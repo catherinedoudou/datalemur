67f40b5346b05b2ce14263754e76786bb67957cc
cdrowley <49714796+cdrowley@users.noreply.github.com>
2023-06-05 14:03:54 +0100

    -

--- questions/sql/medium/signup-confirmation-rate.sql
+++ questions/sql/medium/signup-confirmation-rate.sql
@@ -1,2 +1,7 @@
 -- https://datalemur.com/questions/questions/signup-confirmation-rate
+SELECT ROUND(COUNT(t . *)::DECIMAL / COUNT(e . *), 2)
+FROM emails e
+LEFT JOIN texts t
+  ON t.email_id = e.email_id
+  AND t.signup_action = 'Confirmed'
 ;
