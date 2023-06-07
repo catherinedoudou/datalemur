0e3280ba8337b979721fe9bc3376906f8c5c7b64
cdrowley <49714796+cdrowley@users.noreply.github.com>
2023-06-07 17:34:38 +0100

    -

--- sql/hard/updated-status.sql
+++ sql/hard/updated-status.sql
@@ -1,2 +1,14 @@
 -- https://datalemur.com/questions/updated-status
-SELECT NULL AS blank;
+SELECT
+  COALESCE(a.user_id, dp.user_id) AS user_id
+  , CASE
+    WHEN a.user_id IS NULL THEN 'NEW'
+    WHEN dp.user_id IS NULL THEN 'CHURN'
+    WHEN a."status" = 'CHURN' THEN 'RESURRECT'
+    ELSE 'EXISTING'
+  END AS new_status
+FROM advertiser a
+FULL OUTER JOIN daily_pay dp
+  ON a.user_id = dp.user_id
+ORDER BY 1
+;
