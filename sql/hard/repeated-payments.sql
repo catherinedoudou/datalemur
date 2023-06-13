3f0df68a663558b326c20ef155a6845ceacb2248
cdrowley <49714796+cdrowley@users.noreply.github.com>
2023-06-13 01:16:32 +0100

    -

--- sql/hard/repeated-payments.sql
+++ sql/hard/repeated-payments.sql
@@ -1,2 +1,10 @@
 -- https://datalemur.com/questions/repeated-payments
-SELECT NULL AS blank;
+WITH prev_transactions AS (
+  SELECT transaction_timestamp - LAG(transaction_timestamp) OVER (PARTITION BY merchant_id, credit_card_id, amount ORDER BY transaction_timestamp) AS interval_diff
+  FROM "transactions"
+)
+
+SELECT COUNT(*) AS payment_count
+FROM prev_transactions
+WHERE interval_diff <= INTERVAL '10 MINUTES'
+;
