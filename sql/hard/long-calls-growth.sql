74be356892907b108501dfe3750f1e4fbe181c6c
cdrowley <49714796+cdrowley@users.noreply.github.com>
2023-06-17 19:16:06 +0100

    -

--- sql/hard/long-calls-growth.sql
+++ sql/hard/long-calls-growth.sql
@@ -1,2 +1,20 @@
 -- https://datalemur.com/questions/long-calls-growth
-SELECT NULL AS blank;
+WITH long_calls_by_year_month AS (
+  SELECT
+    EXTRACT('YEAR' FROM call_received) AS yr
+    , EXTRACT('MONTH' FROM call_received) AS mth
+    , COUNT(*) FILTER (WHERE call_duration_secs > 300) AS calls
+  FROM callers
+  GROUP BY 1, 2
+)
+
+SELECT
+  yr
+  , mth
+  , ROUND(
+    100.0 * (calls - LAG(calls) OVER w) / LAG(calls) OVER w
+    , 1
+  ) AS growth_pct
+FROM long_calls_by_year_month
+WINDOW w AS (ORDER BY yr, mth)
+;
